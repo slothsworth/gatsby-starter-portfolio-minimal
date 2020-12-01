a368a33896ab1fefe381a33ec66aff7f62b3f035
Javier Dominguez <javierdominguez09@gmail.com>
2020-11-30 19:48:42 -0500

    added titles

--- config/index.js
+++ config/index.js
@@ -1,67 +1,68 @@
 module.exports = {
+  author: "@javierdominguez",
+  siteTitle: "Javier Dominguez Portfolio",
+  siteShortTitle: "jd.", // Used as logo text in header, footer, and splash screen
+  siteDescription:
+    "A modern one-page portfolio with a clean yet expressive design.",
+  siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
+  siteLanguage: "en_US",
+  siteIcon: "content/favicon.png", // Relative to gatsby-config file
+  seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
 
-    author: "@javierdominguez",
-    siteTitle: "Javier Dominguez Portfolio",
-    siteShortTitle: "jd.", // Used as logo text in header, footer, and splash screen
-    siteDescription: "A modern one-page portfolio with a clean yet expressive design.",
-    siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
-    siteLanguage: "en_US",
-    siteIcon: "content/favicon.png", // Relative to gatsby-config file
-    seoTitleSuffix: "Portfolio Minimal", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  splashScreen: false, // Set this to true if you want to use the splash screen
 
-    splashScreen: false, // Set this to true if you want to use the splash screen
+  // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+  // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+  mediumRssFeed:
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
+  shownArticles: 3,
 
-    // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
-    // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
-    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
-    shownArticles: 3,
-    
-    // There are icons available for the following platforms: 
-    // Medium, GitHub, LinkedIn, XING, Behance
-    socialMedia: [
-        {
-            name: "Linkedin",
-            url: "https://www.linkedin.com/in/dominguezjavier"
-        },
-        {
-            name: "Medium",
-            url: "https://https://medium.com/@javierdominguez09"
-        },
-        {
-            name: "Github",
-            url: "https://github.com/slothsworth"
-        },
-    ],
-  
-    navLinks: {
-        menu: [
-            {
-                name: "Articles",
-                url: "/#articles",
-            },
-            {
-                name: "About Me",
-                url: "/#about",
-            },
-            {
-                name: "Features",
-                url: "/#projects",
-            },
-        ],
-        button: {
-            name: "Contact",
-            url: "/#contact",
-        }
+  // There are icons available for the following platforms:
+  // Medium, GitHub, LinkedIn, XING, Behance
+  socialMedia: [
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/dominguezjavier",
     },
+    {
+      name: "Medium",
+      url: "https://medium.com/@javierdominguez09",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/slothsworth",
+    },
+  ],
 
-    footerLinks: [
-        {
-            name: "Privacy",
-            url: "/privacy"
-        },
-        {
-            name: "Imprint",
-            url: "/imprint"
-        }
-    ]
+  navLinks: {
+    menu: [
+      {
+        name: "Articles",
+        url: "/#articles",
+      },
+      {
+        name: "About Me",
+        url: "/#about",
+      },
+      {
+        name: "Features",
+        url: "/#projects",
+      },
+    ],
+    button: {
+      name: "Contact",
+      url: "/#contact",
+    },
+  },
+
+  footerLinks: [
+    {
+      name: "Privacy",
+      url: "/privacy",
+    },
+    {
+      name: "Imprint",
+      url: "/imprint",
+    },
+  ],
 }
